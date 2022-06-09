62fa5232cd44a5f9bb7cf50765ac13b6da5eab08
Elmansoory <100771018+Tiijan@users.noreply.github.com>
2022-06-09 12:28:17 +0200

    dawdwa

--- footer.php
+++ footer.php
@@ -4,20 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="footer.css">
+    
+    <link rel="stylesheet" href="fs.css">
     <title>4 season hotel</title>
 </head>
 <body>
-  <div id="right">
-    <h5>More</h5>
-    <p>PRIVATE JET</p>
-    <p>RESIDENCES</p>
-    <p>RESIDENCES</p>
-    <p>PRIVATE RETREATS</p>
-    <p>GIFT CARDS</p>
+
+   <div id="right">
+    <h3>More</h3>
+    <a href="https://www.fourseasons.com/privatejet/" id="mar">PRIVATE JET</a>
+    <br>
+    <a href="https://www.fourseasons.com/residences/private_residences/" id="mar">RESIDENCES</a>
+    <br>
+    <a href="https://www.fourseasons.com/privateretreats/" id="mar">PRIVATE RETREATS</a>
+    <br>
+    <a id="mar" href="https://fourseasons.buyatab.com/custom/fourseasons/giftcard/?_ga=2.45669165.847659181.1654760628-168061361.1654760628&_gl=1*10y2b3s*_ga*MTY4MDYxMzYxLjE2NTQ3NjA2Mjg.*_ga_Q9N1LB2PM5*MTY1NDc2OTkzOS40LjEuMTY1NDc3MDAwNy42MA..">GIFT CARDS</a>
   </div>
   <div id="rightt">
-    <h5>News</h5>
+    <h3>News</h3>
     <p>PRESS ROOM</p>
     <p>NEW OPENINGS</p>
     <p>MAGAZINE</p>
@@ -35,18 +39,20 @@
     <p>CAREERS</p>
     <p>CONTACT US</p>    
   </div>
+
+  
     <div id="img">
         <a href="" id="ima1">
-            <img src="" alt="">
+            <img src="twitter.png" alt="">
         </a>
         <a href="" id="ima2">
-            <img src="" alt="">
-        </a>
-        <a href="" id="ima3">
-            <img src="" alt="">
+            <img src="facebook.png" alt="">
         </a>
     </div>
-    
+    <hr id="hrs">
+    <p id="pp">
+    ©project 2022. All Rights Reserved.
+    </p>  
   
 </body>
 </html>
