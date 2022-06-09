0027bb66dcea9615ef1146eaf18ccc6a3a404add
Elmansoory <100771018+Tiijan@users.noreply.github.com>
2022-06-09 11:35:03 +0200

    ad

---
+++ footer.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
+    <title>4 season hotel</title>
+</head>
+<body>
+  <div id="right">
+    <h5>More</h5>
+    <p>PRIVATE JET</p>
+    <p>RESIDENCES</p>
+    <p>RESIDENCES</p>
+    <p>PRIVATE RETREATS</p>
+    <p>GIFT CARDS</p>
+  </div>
+  <div id="rightt">
+    <h5>News</h5>
+    <p>PRESS ROOM</p>
+    <p>NEW OPENINGS</p>
+    <p>MAGAZINE</p>
+    <p>NEWSLETTER</p>    
+  </div>
+  <div id="righttt">
+    <h5>Reservations</h5>
+    <p>REQUEST AN INVOICE</p>
+    <p>FIND A RESERVATION</p>
+    <p>EMAIL PREFERENCES</p>    
+  </div>
+  <div id="left">
+    <h5>About</h5>
+    <p>ABOUT US</p>
+    <p>CAREERS</p>
+    <p>CONTACT US</p>    
+  </div>
+    <div id="img">
+        <a href="" id="ima1">
+            <img src="" alt="">
+        </a>
+        <a href="" id="ima2">
+            <img src="" alt="">
+        </a>
+        <a href="" id="ima3">
+            <img src="" alt="">
+        </a>
+    </div>
+    
+  
+</body>
+</html>
