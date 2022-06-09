7f98e8f87f08f9a2b8c2b8bc61bcdab6e2d5b053
SE7EN404 <100765912+SE7EN404@users.noreply.github.com>
2022-06-09 13:29:44 +0200

    c
    fv

--- Form_Delete.php
+++ Form_Delete.php
@@ -1,3 +1,23 @@
 <?php 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>rent car</title>
+    <link rel="stylesheet" href="Form_Delete.css">
+</head>
+<h1 class="delete"> delete order </h1>
+   <body>
+     
+
+      <label id="label"for="ID">ID</label>
+    <input id="id" type="text" id="ID" name="ID" placeholder="Your id pls..">
+
+  </form>
+</div>
+</body>
+</html>
